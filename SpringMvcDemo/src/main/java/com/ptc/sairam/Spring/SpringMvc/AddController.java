ca75e98cfc09e260279d636a5a4e17fea4a1705b
Sairam Madem <smadem@ptc.com>
2019-08-09 14:56:12 -0400

    A sample servlet is added.

---
+++ SpringMvcDemo/src/main/java/com/ptc/sairam/Spring/SpringMvc/AddController.java
@@ -0,0 +1,26 @@
+package com.ptc.sairam.Spring.SpringMvc;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class AddController {
+	
+	@RequestMapping("/add")
+	public ModelAndView Add(HttpServletRequest req, HttpServletResponse resp) {
+		int i = Integer.parseInt(req.getParameter("t1"));
+		int j = Integer.parseInt(req.getParameter("t2"));
+		
+		int k = i + j;
+		
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("display.jsp");
+		mv.addObject("result", k);
+		
+		return mv;
+	}
+}
