60f711fdd350077f8263843b6ed531dc882ebc23
Sairam Madem <smadem@ptc.com>
2019-08-19 15:22:41 -0400

    Test class is added for Rest controller

---
+++ OreillyDemo/src/main/java/com/oreilly/demo/controllers/HelloRestController.java
@@ -0,0 +1,15 @@
+package com.oreilly.demo.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.oreilly.demo.entities.Greeting;
+
+@RestController
+public class HelloRestController {
+	@GetMapping("/rest")
+	public Greeting greet(@RequestParam(required = false, defaultValue = "World") String name) {
+		return new Greeting("Hello, " + name + "!");
+	}
+}
