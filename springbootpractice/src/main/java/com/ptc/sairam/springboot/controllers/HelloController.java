64bef28b8edd3d77e88d789df95cf0261169046e
Sairam Madem <smadem@ptc.com>
2019-11-22 22:49:33 -0500

    Project for Oreilly Spring course by Ken Kousen. Refer: https://github.com/kousen/spring-and-spring-boot/wiki

---
+++ springbootpractice/src/main/java/com/ptc/sairam/springboot/controllers/HelloController.java
@@ -0,0 +1,19 @@
+package com.ptc.sairam.springboot.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller()
+public class HelloController {
+	
+	@GetMapping("/hello")
+	public String sayHello(
+			@RequestParam(required = false, defaultValue = "World"
+	) String name, Model model) {
+		model.addAttribute("user", name);
+		return "hello";
+	}
+}
---
+++ springbootpractice/src/test/java/com/ptc/sairam/springboot/SpringbootpracticeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ptc.sairam.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class SpringbootpracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
